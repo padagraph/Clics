be583f74c7da016f4d5506386472b6c7c2a80fff
pierre <pierre@magistry.fr>
2018-06-08 08:49:51 +0800

    init

---
+++ gml_to_csv.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+import igraph
+import sys
+import codecs
+
+NODES_HEADERS = ["@Concept: #id", "key", "label", "concept", "body_part", "frequency"]
+
+EDGES_HEADERS = ["_Edge:", "languages", "families", "weight"]
+
+
+
+if __name__ == "__main__":
+    input_file = "/home/pierre/Téléchargements/clics.gml" #  sys.argv[1]
+    nodes_file = "./nodes.csv" #  sys.argv[2]
+    edges_file = "./edges.csv" #  sys.argv[3]
+    g = igraph.load(input_file)
+    with codecs.open(nodes_file, "w", "utf-8") as f:
+        f.write(", ".join(NODES_HEADERS))
+        f.write("\n")
+        for v in g.vs():
+            v['id'] = int(v['id'])
+            line =  [str(v[x]).replace(",","/") for x in  ["id"] + NODES_HEADERS[1:]]
+            f.write(", ".join(line))
+            f.write("\n")
+    with codecs.open(edges_file, "w", "utf-8") as f:
+        f.write(", ".join(EDGES_HEADERS))
+        f.write("\n")
+        for e in g.es():
+            src = str(g.vs[e.source]['id'])
+            tgt = str(g.vs[e.target]['id'])
+            line = src + " -- " + tgt + ", " + (", ".join([str(e[x]) for x in EDGES_HEADERS[1:]])) + "\n"
+            f.write(line)
+
